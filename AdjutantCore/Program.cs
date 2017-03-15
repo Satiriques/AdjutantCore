ee551795725e04a946fc78a59a44fa0c35f49132
Satiriques <bad_ghost2005@hotmail.com>
2017-03-15 14:51:34 -0400

    Add project files.

---
+++ AdjutantCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdjutantCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
